494e587be0833c0c29d4ffa77530e542a7ae2676
chanpark <park.chantaek@gmail.com>
2022-10-02 12:17:06 -0400

    Sizeof with side effects

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
+int f(void)
+{
+    int i = 0;
+    char arr[20];
+    int size = sizeof(arr[i++]); //wrong: sizeof expression has side effect
+    std::cout << i; //would output 0 instead of 1
+}
+
 int main(int argc, char** argv)
 {
     std::cout << "Hello World" << std::endl;
+    f();
     return 0;
 }
